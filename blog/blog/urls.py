680812a210c5e73820e5cd794f5b2bd73f2515f5
gye200 <Guoliang_Ye@cable.comcast.com>
2018-06-26 17:36:47 -0700

    updates

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
+from blogpost import views
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index),
+    url(r'^blog/(?P<slug>[^\.]+).html', views.view_post, name='view_blog_post'),
+    url(r'^admin/', include(admin.site.urls))
 ]
